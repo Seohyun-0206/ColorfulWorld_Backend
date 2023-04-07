f9619192a10a2e9a85014a5b18b0ecf1e5849262
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-07 15:00:13 +0900

    feat : UserController에 register 추가

--- src/main/java/com/example/Colorful_World/controller/UserController.java
+++ src/main/java/com/example/Colorful_World/controller/UserController.java
@@ -1,18 +1,31 @@
 package com.example.Colorful_World.controller;
 
 
+import com.example.Colorful_World.Dto.UserDto;
+import com.example.Colorful_World.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(){
+    private final UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody Map<String, String> param){
+
+        UserDto userDto = new UserDto(param.get("email"),
+                param.get("password"),
+                Integer.parseInt(param.get("index")));
+
+
+        userService.register(userDto);
 
         return ResponseEntity.ok("회원가입에 성공하였습니다.");
     }
