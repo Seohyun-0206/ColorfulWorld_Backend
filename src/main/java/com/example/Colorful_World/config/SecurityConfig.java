8ac88a2bbd5b0115e1aea5a843bb5d3f4caac593
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-08 00:17:42 +0900

    feat : 비밀번호 encoding 추가

---
+++ src/main/java/com/example/Colorful_World/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.Colorful_World.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    //비밀번호 인코딩
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 정적 자원에 스프링 시큐리티 필터 규칙을 적용하지 않도록 설정
+        return (web) -> web.ignoring().requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations()));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .headers().frameOptions().disable()
+                .and()
+
+                .authorizeHttpRequests()
+                .requestMatchers( "/register").permitAll() //설정한 리소스의 접근을 인증 절차 없이 모두 허용한다.
+                .anyRequest().authenticated() //그 외 나머지 리소스는 인증된 사용자만 접근할 수 있다.
+
+        ;
+
+        return http.build();
+
+    }
+}
--- src/main/java/com/example/Colorful_World/controller/UserController.java
+++ src/main/java/com/example/Colorful_World/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.Colorful_World.dto.UserDto;
 import com.example.Colorful_World.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +18,14 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     @ResponseBody
     public ResponseEntity<String> register(@RequestBody Map<String, String> param){
 
         UserDto userDto = new UserDto(param.get("email"),
-                param.get("password"),
+                passwordEncoder.encode(param.get("password")),
                 Integer.parseInt(param.get("intensity")));
 
 
