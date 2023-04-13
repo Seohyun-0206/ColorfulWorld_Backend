476d79491acb18812adca6b59f1b709ad6ab362c
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-13 21:02:05 +0900

    feat : token 생성 코드 추가

---
+++ src/main/java/com/example/Colorful_World/token/JwtTokenProvider.java
@@ -0,0 +1,73 @@
+package com.example.Colorful_World.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    @Value("${spring.jwt.secretkey}")
+    private String secretKey;
+
+    //토큰 유효 시간 설정
+    private Long accessTime = 1 * 60 * 1000L;
+    private Long refreshTime = 5 * 60 * 1000L;
+
+    private Key key;
+
+
+    //secretKey encoding
+    @PostConstruct
+    protected void init(){
+        byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
+        key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    //토큰 생성
+    public String createToken(String email, String type){
+
+        Date now = new Date();
+
+        //토큰 종류에 따른 시간 설정
+        long time = type.equals("access") ? accessTime : refreshTime;
+
+        //Payload
+        Claims claims = Jwts.claims()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + time));
+
+        claims.put("type", type);
+
+
+        return Jwts.builder()
+                .setHeader(createHeader())
+                .setClaims(claims)
+                .signWith(key, SignatureAlgorithm.forSigningKey(key))
+                .compact();
+    }
+
+    private Map<String, Object> createHeader(){
+        Map<String, Object> header = new HashMap<>();
+        header.put("typ", "JWT");
+        header.put("alg", "HS256");
+
+        return header;
+    }
+
+}
