7cb3006715e8fdb2abcaf55bb3884554c8d16e92
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-13 21:55:20 +0900

    feat : filter 생성

---
+++ src/main/java/com/example/Colorful_World/token/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.example.Colorful_World.token;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.GenericFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
+
+
+        chain.doFilter(request, response);
+    }
+}
