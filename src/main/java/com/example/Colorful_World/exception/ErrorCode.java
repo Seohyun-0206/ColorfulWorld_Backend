21d07b8d644fce2e1492597a098964712cb6548b
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-13 20:13:17 +0900

    feat : login  정보 확인 ㅋ코드 추가

--- src/main/java/com/example/Colorful_World/exception/ErrorCode.java
+++ src/main/java/com/example/Colorful_World/exception/ErrorCode.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    /*400:BAD REQUEST*/
+    NO_USER(HttpStatus.BAD_REQUEST, "LOGIN-001", "해당 유저가 없습니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "LOGIN-002", "비밀번호가 일치하지 않습니다."),
+
     /*409:CONFLICT*/
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "REGISTER-001", "중복된 이메일이 존재합니다."),
 
--- src/main/java/com/example/Colorful_World/service/UserService.java
+++ src/main/java/com/example/Colorful_World/service/UserService.java
@@ -3,10 +3,12 @@ package com.example.Colorful_World.service;
 
 import com.example.Colorful_World.dto.LoginDto;
 import com.example.Colorful_World.dto.UserDto;
+import com.example.Colorful_World.entity.UserEntity;
 import com.example.Colorful_World.exception.BaseException;
 import com.example.Colorful_World.exception.ErrorCode;
 import com.example.Colorful_World.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void register(UserDto userDto){
@@ -32,6 +35,17 @@ public class UserService {
 
     @Transactional
     public void login(LoginDto loginDto){
+
+        //회원이 존재하는지 확인
+        UserEntity userEntity = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(
+                ()-> new BaseException(ErrorCode.NO_USER)
+        );
+
+        if(!passwordEncoder.matches(loginDto.getPassword(), userEntity.getPassword())){
+            throw new BaseException(ErrorCode.PASSWORD_MISMATCH);
+        }
+
+
 
     }
 
