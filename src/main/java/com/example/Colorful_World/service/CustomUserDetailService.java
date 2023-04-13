0f2f14dde2e6e434b9087e0ed4abd3abb1861ad2
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-04-13 21:37:03 +0900

    feat : CustomUserDetailService 생성

---
+++ src/main/java/com/example/Colorful_World/service/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package com.example.Colorful_World.service;
+
+import com.example.Colorful_World.entity.UserEntity;
+import com.example.Colorful_World.repository.UserRepository;
+import com.example.Colorful_World.token.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        return customUserDetails;
+    }
+}
