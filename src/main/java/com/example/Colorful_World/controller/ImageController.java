41ab6278a5571b54316d28503fa77af405ab5274
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-05-06 20:06:38 +0900

    feat : ImageController 작성

---
+++ src/main/java/com/example/Colorful_World/controller/ImageController.java
@@ -0,0 +1,26 @@
+package com.example.Colorful_World.controller;
+
+import com.example.Colorful_World.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/image")
+    @ResponseBody
+    public ResponseEntity<String> imageSave(@RequestParam("image")MultipartFile img){
+
+        imageService.saveImage(img);
+
+        return ResponseEntity.ok("이미지 저장에 성공하였습니다.");
+    }
+}
---
+++ src/main/java/com/example/Colorful_World/service/ImageService.java
@@ -0,0 +1,10 @@
+package com.example.Colorful_World.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageService {
+
+    public void saveImage(MultipartFile img){
+
+    }
+}
